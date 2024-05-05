bb242ec5a7b19ccf22e1c04350cde52909778165
Fan223 <fanj.zh@foxmail.com>
2024-05-05 09:14:17 +0800

    init

---
+++ src/main/java/fable/FableGatewayApplication.java
@@ -0,0 +1,17 @@
+package fable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gateway startup class
+ *
+ * @author Fable
+ * @since 2024/5/5 9:12
+ */
+@SpringBootApplication
+public class FableGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FableGatewayApplication.class, args);
+    }
+}
