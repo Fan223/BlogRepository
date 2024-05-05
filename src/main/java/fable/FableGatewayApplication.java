5d122cbf5b31275178d7789d9290768e1d95b501
Fan223 <fanj.zh@foxmail.com>
2024-05-05 09:40:18 +0800

    init

--- src/main/java/fable/FableGatewayApplication.java
+++ src/main/java/fable/FableGatewayApplication.java
@@ -2,6 +2,7 @@ package fable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Gateway startup class
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2024/5/5 9:12
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FableGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(FableGatewayApplication.class, args);
